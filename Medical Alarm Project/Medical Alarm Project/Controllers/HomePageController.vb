d55e0a68eeb7ccd2302b23ee3ddf096434f97935
Brandon Wong <bwong36@gmail.com>
2012-11-16 22:28:40 -0800

    The very start

---
+++ Medical Alarm Project/Medical Alarm Project/Controllers/HomePageController.vb
@@ -0,0 +1,13 @@
+﻿Namespace Medical_Alarm_Project
+    Public Class HomePageController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /HomePage
+
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace
---
+++ Medical Alarm Project/Medical Alarm Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Medical_Alarm_Project.MvcApplication" Language="VB" %>
