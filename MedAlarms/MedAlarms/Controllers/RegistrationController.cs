aed0f301573a90938c4a2a26f79618ee40a745b0
Brandon Wong <bwong36@gmail.com>
2012-11-22 18:54:40 -0800

    Trying to make a registration page and failing
    Having troubles making the registration page viewable globally

---
+++ MedAlarms/MedAlarms/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedAlarms.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        //
+        // GET: /SignIn/
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
