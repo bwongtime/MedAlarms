ae9a91e6c958ad50de4229c0573712e65ca922d3
Brandon Wong <bwong36@gmail.com>
2012-11-30 01:26:53 -0800

    Adding Alegria's website

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Medical Alarm Database";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Walkthur()
+        {
+            return View();
+        }
+        public ActionResult Annotate()
+        {
+            return View();
+        }
+    }
+}
